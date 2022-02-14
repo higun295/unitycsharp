c8a676f4c26c66ffc7884a495966d1ee40b9cf3b
HEEKWON <higun295@gmail.com>
2022-02-14 09:21:05 +0900

    20220214_1

---
+++ UnityCSharp/UnityCSharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UnityCSharp {
+    class Program {
+        static void Main(string[] args) {
+            int a = 0x11;
+            int b = 0b11;
+            int c = 11;
+
+            Console.WriteLine($"Hexa : {a}, Binary : {b}, Dec : {c}");
+
+            bool isA = true;
+            bool isB = false;
+
+            Console.WriteLine($"{Convert.ToInt32(isA)},,,,,{Convert.ToInt32(isB)}");
+        }
+    }
+}
