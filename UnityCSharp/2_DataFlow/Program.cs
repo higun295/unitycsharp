8c45bd48ea564351f0e4fdd77628526b60270aef
HEEKWON <higun295@gmail.com>
2022-04-22 12:06:03 +0900

    20220422_1

--- UnityCSharp/2_DataFlow/Program.cs
+++ UnityCSharp/2_DataFlow/Program.cs
@@ -14,15 +14,40 @@ namespace _2_DataFlow
             //Program.Add(1, 2, d: 2.0f);
             //Console.WriteLine(ret);
 
-            int a = 0;
-            Program.AddOne(a);
+            //int a = 0;
 
-            Console.WriteLine(a);
+            //// 두 가지 기능은 같은데 return을 받는 것이 좀 낫겠다.
+            //Program.AddOne(ref a);
+            //Console.WriteLine(a);
+
+            //a = Program.AddOne2(a);
+            //Console.WriteLine(a);
+
+            int num1 = 10;
+            int num2 = 3;
+
+            int result1;
+            int result2;
+            Divide(num1, num2, out result1, out result2);
+
+            Console.WriteLine(result1);
+            Console.WriteLine(result2);
         }
 
-        static void AddOne(int number)
+        static void Divide(int a, int b, out int result1, out int result2)
+        {
+            result1 = a / b;
+            result2 = a % b;
+        }
+
+        static void AddOne(ref int number)
         {
             number = number + 1;
+        }
+
+        static int AddOne2(int number)
+        {
+            return number + 1;
         }
     }
 }
--- UnityCSharp/3_TextRPG/Program.cs
+++ UnityCSharp/3_TextRPG/Program.cs
@@ -4,9 +4,103 @@ namespace _3_TextRPG
 {
     class Program
     {
-        static void Main(string[] args)
+        private enum ClassType
         {
+            None = 0,
+            Knight = 1,
+            Archer = 2,
+            Mage = 3
+        }
 
+        private struct Player
+        {
+            public int hp;
+            public int attack;
+        }
+
+        private static ClassType ChooseClass ()
+        {
+            Console.WriteLine("직업을 선택하세요!");
+            Console.WriteLine("[1] 기사");
+            Console.WriteLine("[2] 궁수");
+            Console.WriteLine("[3] 법사");
+
+            ClassType choice = ClassType.None;
+            string input = Console.ReadLine();
+            switch (input)
+            {
+                case "1":
+                    choice = ClassType.Knight;
+                    break;
+                case "2":
+                    choice = ClassType.Archer;
+                    break;
+                case "3":
+                    choice = ClassType.Mage;
+                    break;
+            }
+
+            return choice;
+        }
+
+        private static void CreatePlayer (ClassType choice, out Player player)
+        {
+            switch (choice)
+            {
+                case ClassType.Knight:
+                    player.hp = 100;
+                    player.attack = 10;
+                    break;
+                case ClassType.Archer:
+                    player.hp = 75;
+                    player.attack = 12;
+                    break;
+                case ClassType.Mage:
+                    player.hp = 50;
+                    player.attack = 15;
+                    break;
+                default:
+                    player.hp = 0;
+                    player.attack = 0;
+                    break;
+            }
+        }
+
+        private static void EnterGame ()
+        {
+            while (true)
+            {
+                Console.WriteLine("게임에 접속했습니다!");
+                Console.WriteLine("[1] 필드로 간다.");
+                Console.WriteLine("[2] 로비로 돌아가기");
+
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        // EnterField();
+                        break;
+                    case "2":
+                        return;
+                }
+            }
+        }
+
+        static void Main (string[] args)
+        {
+            while (true)
+            {
+                ClassType choice = ChooseClass();
+                if (choice != ClassType.None)
+                {
+                    // 캐릭터 생성
+                    Player player;
+                    CreatePlayer(choice, out player);
+
+                    // 게임에 접속
+                    EnterGame();
+                }
+            }
         }
     }
 }
